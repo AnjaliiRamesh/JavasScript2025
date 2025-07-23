652c953f3456de045a51f82839c46e8520953834
Anjali <anjaliramesh14012005@gmail.com>
2025-07-23 14:47:37 +0530

    solved operators and quest

---
+++ operator.js
@@ -0,0 +1,76 @@
+
+// Assignment operator : it assigns value with oper = 
+// var x = 5;
+// var y = 9;
+
+// Arithmetic operators
+// var x = 2;
+// var y = 4;
+// var res1 = x+y;
+// var res2 = y-x;
+// var res3 = x*y;
+// var res4 = y/x;
+// var res5 = y%x;//gets remainders
+
+
+// console.log(res1);
+// console.log(res2);
+// console.log(res3);
+// console.log(res4);
+// console.log(res5);
+
+//  var res = "hello" / 2;
+//  console.log(res); //NaN 
+ 
+//  var reres = 0.1 + 0.2;
+//  console.log(reres);
+ 
+// 
+// Comparison operator
+// equal to (==)
+// it does not check data type
+// console.log(5 == 5);
+// console.log(5 == "5");
+
+
+// strict equal to (===)
+// it checks for datatypes also
+// console.log( 5 === 5);
+// console.log( 5 === "5");
+
+// not equal to (!=)
+// console.log(5 != 5);
+
+ // grater than and less the
+// console.log(5 > 8);
+// console.log(5 < 8);
+
+// Logical operator
+// && -> true when both are true
+//  || -> true if either is true
+// var x = 5;
+// var y = 10;
+// console.log(x>0 && y<0);
+// console.log(x>0 || y<0);
+
+// // Logical no (!) 
+// var isopen = true;
+// console.log(!isopen);
+
+
+// check valid age
+// var age = 14;
+// var dive = true;
+
+// if(age >= 18){
+//     console.log(dive);   
+// }else{
+//     console.log(!dive);   
+// }
+
+// console.log(age>=18 && dive);
+// console.log(age>=18 || dive);
+
+
+// Ternary operator
+
