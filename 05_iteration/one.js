ab451b600eaac1f964f666129f26b96a755ed65d
Anjali <anjaliramesh14012005@gmail.com>
2025-09-19 16:19:33 +0530

    if else

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -14,40 +14,67 @@
 
 // console.log(element);
 
-for (let i = 1; i <= 10; i++) {
-    // console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
-    // console.log(`Inner loop value ${j} and inner loop ${i}`);
-    // console.log(i + '*' + j + ' = ' + i*j );
-   }
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop value: ${i}`);
+//    for (let j = 1; j <= 10; j++) {
+//     // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     // console.log(i + '*' + j + ' = ' + i*j );
+//    }
     
-}
-let myArray = ["flash", "batman", "superman"]
-//console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    // console.log(element);
+// }
+// let myArray = ["flash", "batman", "superman"]
+// //console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element);
     
-}
+// }
 
 
 // break and continue
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected so stop at 5`);
-        break
-    }
-   console.log(`Value of i is ${index}`);
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected so stop at 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
     
-}
+// }
 
-for (let index = 1; index <= 10; index++) {
-    if (index == 5) {
-        console.log(`Detected 5 continue`);
-        continue
-    }
-   console.log(`Value of i is ${index}`);
+// for (let index = 1; index <= 10; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5 continue`);
+//         continue
+//     }
+//    console.log(`Value of i is ${index}`);
     
-}
+// }
 
+// let a = "red";
+// let b = "yellow";
+// let c = "green";
+// if(a){
+//     console.log("stop"); 
+// }
+// if(b){
+//     console.log("wait");
+    
+// }
+// if(c){
+//     console.log("go");
+    
+// }
+
+// let color = "red";
+let color = "yellow";
+// let color = "green";
+if(color == "red"){
+    console.log("stop");  
+}
+if(color == "yellow"){
+    console.log("wait");  
+}
+if(color == "green"){
+    console.log("go");  
+}
