ae4686f4369f2900c5fb8338f4c8c46132143ed6
Anjali <anjaliramesh14012005@gmail.com>
2025-12-25 13:01:50 +0530

    december1

---
+++ Dec/01variable.js
@@ -0,0 +1,19 @@
+
+const accountId = 144553;
+let accountEmail = "anjali@stack.com";
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState ;
+
+
+// console.log(accountId);  
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// accountId = 12343;
+accountEmail = "pihu@stack.com";
+accountPassword = "1223345"
+accountCity = "delhi"
+accountState ;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,6 @@
 let myCreatedDate = new Date(2023, 0, 23)
 let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 let myCreatedDate = new Date("2023-01-14")
+
+Day 1
+
