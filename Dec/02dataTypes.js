434b64ddce9d641ed1d42d1997908a074673a7d4
Anjali <anjaliramesh14012005@gmail.com>
2025-12-30 11:57:48 +0530

    day2

---
+++ Dec/02dataTypes.js
@@ -0,0 +1,10 @@
+let name = "anjali"
+let age = 21
+let state;
+let city = null;
+let ans = true;
+
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(state));//undefined
+console.log(typeof(city));//object
