54f0b316add74b00142d12e04b097c1c24885bce
Anjali <anjaliramesh14012005@gmail.com>
2025-12-30 12:32:12 +0530

    day2

--- Dec/02dataTypes.js
+++ Dec/02dataTypes.js
@@ -4,7 +4,32 @@ let state;
 let city = null;
 let ans = true;
 
-console.log(typeof(age));
-console.log(typeof(name));
-console.log(typeof(state));//undefined
-console.log(typeof(city));//object
+// console.log(typeof(age));
+// console.log(typeof(name));
+// console.log(typeof(state));//undefined
+// console.log(typeof(city));//object
+
+
+
+let value = 3
+let negVal = -value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/2); 
+// console.log(2%2);
+
+let str1 = 'hello'
+let str2 = 'anjali'
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);//12
+console.log("1" + "2");//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
