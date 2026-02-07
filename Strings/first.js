4a1748ed953cdba0c49d48e821753860cfd0b79c
Anjali <anjaliramesh14012005@gmail.com>
2026-02-07 15:47:54 +0530

    day one done

--- Strings/first.js
+++ Strings/first.js
@@ -1,5 +1,37 @@
 let msg = " help!  ";
 
-msg.trim();
-console.log(msg);
+msg.trim();//removes extra space
+// console.log(msg);
+
+let name = "anjali";
+/*
+console.log(name.replace("anjali", "stack"))
+console.log(name.replace("ali", "prali"))
+
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+
+console.log(name.concat(" is a girl"));
+console.log(name.trim());
+
+
+console.log("anjali\"".length)
+
+let s = "anjali"
+console.log(s.toUpperCase());
+
+
+let text = "Hello world, welcome to the universe.";
+let res = text.startsWith("Hello", 1);
+console.log(res);
+
+let text2 = "Hello World";
+let result = text2.endsWith("world");
+console.log(result);
+
+
+let res = "virat scored 100 in last match";
+console.log(res.slice(13, 17));
+
+*/
 
